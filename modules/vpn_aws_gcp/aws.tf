65e0c3b0600cc20737c9e23a72e8ac4bec4c7d3d
Chris Sng <chrissng@users.noreply.github.com>
2019-07-15 10:12:44 +0800

    dry a bit

--- modules/vpn_aws_gcp/aws.tf
+++ modules/vpn_aws_gcp/aws.tf
@@ -1,3 +1,7 @@
+locals {
+  aws_vpn_gateway_id = var.use_existing_aws_vgw ? data.aws_vpn_gateway.existing[0].id : aws_vpn_gateway.a[0].id
+}
+
 resource "aws_vpn_gateway" "a" {
   count = var.use_existing_aws_vgw ? 0 : 1
 
@@ -9,7 +13,7 @@ resource "aws_vpn_gateway" "a" {
 resource "aws_vpn_gateway_route_propagation" "a" {
   count = length(data.aws_route_tables.tables.ids)
 
-  vpn_gateway_id = var.use_existing_aws_vgw ? data.aws_vpn_gateway.existing[0].id : aws_vpn_gateway.a[0].id
+  vpn_gateway_id = local.aws_vpn_gateway_id
   route_table_id = element(tolist(data.aws_route_tables.tables.ids), count.index)
 }
 
@@ -23,7 +27,7 @@ resource "aws_customer_gateway" "a" {
 }
 
 resource "aws_vpn_connection" "a" {
-  vpn_gateway_id      = var.use_existing_aws_vgw ? data.aws_vpn_gateway.existing[0].id : aws_vpn_gateway.a[0].id
+  vpn_gateway_id      = local.aws_vpn_gateway_id
   customer_gateway_id = aws_customer_gateway.a.id
   type                = "ipsec.1"
   static_routes_only  = false
@@ -39,7 +43,7 @@ resource "aws_customer_gateway" "b" {
 }
 
 resource "aws_vpn_connection" "b" {
-  vpn_gateway_id      = var.use_existing_aws_vgw ? data.aws_vpn_gateway.existing[0].id : aws_vpn_gateway.a[0].id
+  vpn_gateway_id      = local.aws_vpn_gateway_id
   customer_gateway_id = aws_customer_gateway.b.id
   type                = "ipsec.1"
   static_routes_only  = false
